6ff7bca0a86973b7dac644c13b728f109a2c74eb
itbra <Tinobee@gmail.com>
2016-02-25 00:20:36 +0100

    Some global tests added

--- public/qa/tests-global.js
+++ public/qa/tests-global.js
@@ -1,7 +1,24 @@
 "use strict";
 
 + (function(){
-  jQuery(function($) {
-    $('pre').css({color: 'red'});
+  suite('Global Tests', function() {
+
+      /* Don't apply this test if this lib is loaded from a CDN */
+      test('jQuery library is available', function() {
+          assert(typeof jQuery === 'object');
+      });
+
+      /* Don't apply this test if this lib is loaded from a CDN */
+      test('Twitter Bootstrap library 3 is available', function() {
+          assert(typeof jQuery().emulateTransitionEnd == 'function');
+      });
+
+      test('Document has a valid title', function() {
+          assert(document.title &&
+              document.title.match(/\S/) &&
+              document.title.toUpperCase() !== 'TODO'
+          );
+      });
+
   });
 }());
