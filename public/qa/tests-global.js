aeeb74a9a6b3399f9ca6ea636cf7ad7917b645fa
itbra <Tinobee@gmail.com>
2016-02-25 00:31:46 +0100

    Tests for CDN-libraries disabled

--- public/qa/tests-global.js
+++ public/qa/tests-global.js
@@ -3,15 +3,15 @@
 + (function(){
   suite('Global Tests', function() {
 
-      /* Don't apply this test if this lib is loaded from a CDN */
+      /* Don't apply this test if this lib is loaded from a CDN
       test('jQuery library is available', function() {
           assert(typeof jQuery === 'object');
-      });
+      }); */
 
-      /* Don't apply this test if this lib is loaded from a CDN */
+      /* Don't apply this test if this lib is loaded from a CDN
       test('Twitter Bootstrap library 3 is available', function() {
           assert(typeof jQuery().emulateTransitionEnd == 'function');
-      });
+      }); */
 
       test('Document has a valid title', function() {
           assert(document.title &&
