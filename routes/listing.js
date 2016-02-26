76fdb73f36ade5adb24f7c55fe33b7c3854a2b6f
itbra <Tinobee@gmail.com>
2016-02-26 22:12:24 +0100

    Forms created to add/edit a listing. Per form tests created. Note: The forms are actually not capable of submitting data. The test suites contain tests asserting the existance of form fields. These tests do currently fail and require a fix.

--- routes/listing.js
+++ routes/listing.js
@@ -10,28 +10,111 @@ var dataProvider = require('../lib/dataprovider/listings.js'),
       data: {}
     };
 
-/* GET listing. */
+
+
+/* GET Edit-Form. */
+
+// With id and alias provided
+router.get('/edit/:id?/:alias?', function(req, res) {
+  var id = req.params.id,
+      listing = dataProvider.getListing(id);
+
+  res.render('listing', {
+    title: 'Edit: ' + (listing.title ? listing.title : ''),
+    debug: req.query.debug,
+    test:  req.query.test,
+    pageTestScript: '/qa/tests-listing-form-edit.js',
+    url: {
+      path:    req.path,
+      url:     req.url,
+      route:   req.route || req.url,
+      query:   query.stringify(req.query),
+      referer: req.header('Referer') || req.url
+    },
+    showForm: true,
+    form: {
+      name: 'edit',
+      data: listing
+    }
+  });
+});
+
+// With id only provided
+router.get('/edit/:id?', function(req, res) {
+  var id = req.params.id,
+      listing = dataProvider.getListing(id);
+
+  res.render('listing', {
+    title: 'Edit: ' + (listing.title ? listing.title : ''),
+    debug: req.query.debug,
+    test:  req.query.test,
+    pageTestScript: '/qa/tests-listing-form-edit.js',
+    url: {
+      path:    req.path,
+      url:     req.url,
+      route:   req.route || req.url,
+      query:   query.stringify(req.query),
+      referer: req.header('Referer') || req.url
+    },
+    showForm: true,
+    form: {
+      name: 'edit',
+      data: listing
+    }
+  });
+});
+
+router.get('/edit/*', function(req, res) {
+  //FIXME - render error page
+  res.send('Bad Route');
+});
+
+
+
+/* GET ADD-Form */
+
+router.get('/add', function(req, res) {
+  res.render('listing', {
+    title: 'Add Listing',
+    debug: req.query.debug,
+    test:  req.query.test,
+    pageTestScript: '/qa/tests-listing-form-add.js',
+    url: {
+      path:    req.path,
+      url:     req.url,
+      route:   req.route || req.url,
+      query:   query.stringify(req.query),
+      referer: req.header('Referer') || req.url
+    },
+    showForm: true,
+    form: {
+      name: 'add',
+      data: {}
+    }
+  });
+});
+
+
+
+/* GET listing details. */
 
 // With id and alias provided
 router.get('/:id?/:alias?', function(req, res) {
-  var id = req.params.id;
-  /* var query2 = req.url.match(/\?/) ? req.url.split('?').pop() : '';
-      query2 = query2.length ? '?' + query2 : query2; */
+  var id = req.params.id,
+      listing = dataProvider.getListing(id);
 
   params.title = 'Listing' + (isNaN(id) ? '' : ' #' + id);
   params.debug = req.query.debug;
   params.test  = req.query.test;
   params.url   = {
-    protocol:  req.protocol,
-    host:      req.host,
-    port:      req.port,
-    path:      req.path,
-    url:       req.url,
-    origURL:   req.originalURL,
-    route:     req.route || req.url,
-    query:     query.stringify(req.query)
+    path:    req.path,
+    url:     req.url,
+    route:   req.route || req.url,
+    query:   query.stringify(req.query),
+    referer: req.header('Referer') || req.url
   };
-  params.data.listing = dataProvider.getListing(id);
+  params.data.listing = listing;
+  params.showForm = false;
 
   if (params.debug === '1') {
     params.queryJSON  = JSON.stringify(req.query);
@@ -44,25 +127,21 @@ router.get('/:id?/:alias?', function(req, res) {
 
 // With id only provided
 router.get('/:id?', function(req, res) {
-  var id = req.params.id;
-  /* var query2 = req.url.match(/\?/) ? req.url.split('?').pop() : '';
-      query2 = query2.length ? '?' + query2 : query2; */
+  var id = req.params.id,
+      listing = dataProvider.getListing(id);
 
   params.title = 'Listing' + (isNaN(id) ? '' : ' #' + id);
   params.debug = req.query.debug;
   params.test  = req.query.test;
   params.url   = {
-    protocol:  req.protocol,
-    host:      req.host,
-    port:      req.port,
-    path:      req.path,
-    url:       req.url,
-    origURL:   req.originalURL,
-    route:     req.route || req.url,
-    queryJSON: JSON.stringify(req.query),
-    query:     query.stringify(req.query)
+    path:    req.path,
+    url:     req.url,
+    route:   req.route || req.url,
+    query:   query.stringify(req.query),
+    referer: req.header('Referer') || req.url
   };
-  params.data.listing = dataProvider.getListing(id);
+  params.data.listing = listing;
+  params.showForm = false;
 
   if (params.debug === '1') {
     params.queryJSON  = JSON.stringify(req.query);
@@ -75,25 +154,21 @@ router.get('/:id?', function(req, res) {
 
 // With neither id nor alias provided, last chance: the query string containing id=...
 router.get('/', function(req, res) {
-  var id = req.query.id;
-  /* var query2 = req.url.match(/\?/) ? req.url.split('?').pop() : '';
-      query2 = query2.length ? '?' + query2 : query2; */
+  var id = req.query.id,
+      listing = dataProvider.getListing(id);
 
   params.title = 'Listing' + (isNaN(id) ? '' : ' #' + id);
   params.debug = req.query.debug;
   params.test  = req.query.test;
   params.url   = {
-    protocol:  req.protocol,
-    host:      req.host,
-    port:      req.port,
-    path:      req.path,
-    url:       req.url,
-    origURL:   req.originalURL,
-    route:     req.route || req.url,
-    queryJSON: JSON.stringify(req.query),
-    query:     query.stringify(req.query)
+    path:    req.path,
+    url:     req.url,
+    route:   req.route || req.url,
+    query:   query.stringify(req.query),
+    referer: req.header('Referer') || req.url
   };
-  params.data.listing = dataProvider.getListing(id);
+  params.data.listing = listing;
+  params.showForm = false;
 
   if (params.debug === '1') {
     params.queryJSON  = JSON.stringify(req.query);
--- routes/listings.js
+++ routes/listings.js
@@ -7,29 +7,31 @@ var dataProvider = require('../lib/dataprovider/listings.js'),
 
 /* GET listings. */
 router.get('/', function(req, res) {
-  /* var query2 = req.url.match(/\?/) ? req.url.split('?').pop() : '';
-      query2 = query2.length ? '?' + query2 : query2; */
+  var listings = dataProvider.getListings();
+  var urlQuery = req.query ? query.stringify(req.query) : '';
+      urlQuery = urlQuery.length > 1 ? '?' + urlQuery : '';
 
   res.render('listings', {
     title: 'Listings',
     debug: req.query.debug,
-    test: req.query.test,
+    test:  req.query.test,
     pageTestScript: '/qa/tests-listings.js',
     url: {
-      protocol:  req.protocol,
-      host:      req.host,
-      port:      req.port,
-      path:      req.path,
-      url:       req.url,
-      origURL:   req.originalURL,
-      route:     req.route || req.url,
-      queryJSON: JSON.stringify(req.query),
-      query:     query.stringify(req.query)
+      path:  req.path,
+      url:   req.url,
+      route: req.route || req.url,
+      query: urlQuery
     },
     data: {
-      listings: dataProvider.getListings()
+      listings: listings
     }
   });
+});
+
+// No match. Send error.
+router.get('*', function(req, res) {
+  //FIXME - render error page
+  res.send('Bad Route');
 });
 
 module.exports = router;
