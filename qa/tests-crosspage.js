07d0769b2ba97a783601a0b1c41cd8c69662d995
itbra <Tinobee@gmail.com>
2016-02-28 03:25:37 +1000

    Added cross-page-  and unit tests.

---
+++ qa/tests-crosspage.js
@@ -0,0 +1,11 @@
+"use strict";
+
++ (function(){
+  suite('Cross-Page Tests', function() {
+
+      test('', function() {
+          assert(1 == true);
+      });
+
+  });
+}());
---
+++ qa/tests-unit.js
@@ -0,0 +1,11 @@
+"use strict";
+
++ (function(){
+  suite('Logic Tests', function() {
+
+      test('', function() {
+          assert(1 == true);
+      });
+
+  });
+}());
