9aa200bcc836f473f9a85f80af2e72291e98680d
itbra <Tinobee@gmail.com>
2016-03-01 22:52:15 +0100

    Fixed invalid HbbTV regexp

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,20 @@ var express   = require('express'),
     router    = express.Router();
 
 /* GET index page. */
-router.get('/', function (req, res) { 
+router.get('/', function (req, res) {
   var params = {
     title: '',
     body: 'partials/pages/home.ejs',
-    isHbb: req.headers['user-agent'].match(/(hbbtv/mips(el)?)/i),
+    isHbb: req.headers['user-agent'].match(/(hbbtv|mips(el)?)/i),
     debug: req.query.debug,
     test: req.query.test,
     pageTestScript: '/qa/tests-index.js',
-    allowTestDebug: express().get('env') === 'development',
+    allowTestDebug: (express().get('env') === 'development'),
     url: {
       host: req.host,
       path: req.path,
       url: req.url,
-      route: req.route || req.url,
+      route: (req.route || req.url,
       query: query.stringify(req.query)
     },
     data: {}
